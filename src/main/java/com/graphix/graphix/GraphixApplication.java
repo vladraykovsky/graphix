ba6f400a7726d05b6da8e85fce2894b77da937ee
vlad <vraykovsky2016@gmail.com>
2018-10-03 05:32:11 +0300

    Initail commit

---
+++ src/main/java/com/graphix/graphix/GraphixApplication.java
@@ -0,0 +1,12 @@
+package com.graphix.graphix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphixApplication.class, args);
+    }
+}
---
+++ src/test/java/com/graphix/graphix/GraphixApplicationTests.java
@@ -0,0 +1,16 @@
+package com.graphix.graphix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphixApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
